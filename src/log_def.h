b91169395097f36cb52ce9ef1d6d307200ee42be
leo <leo@qq.com>
2019-07-31 18:32:06 +0800

    完善功能

--- src/log_def.h
+++ src/log_def.h
@@ -18,48 +18,48 @@
 #define L_COND_F(cond, ...)\
 	do{\
 	if(!(cond)){\
-	LogMgr::Obj().PrintfCond(LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "condition fail \[" #cond "]. ", ##__VA_ARGS__); \
+	su::LogMgr::Obj().PrintfCond(su::LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "condition fail \[" #cond "]. ", ##__VA_ARGS__); \
 	return false;\
 	}	\
-	}while(0)
+	}while(0);
 
 //条件非法返回
 #define L_COND(cond, ...)\
 	do{\
 	if(!(cond))	\
 	{\
-	LogMgr::Obj().PrintfCond(LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "condition fail \[" #cond "]. ", ##__VA_ARGS__); \
+	su::LogMgr::Obj().PrintfCond(su::LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "condition fail \[" #cond "]. ", ##__VA_ARGS__); \
 		return; \
 	}\
-	}while(0)
+	}while(0);
 
 #define L_COND_R(cond, ret, ...)\
 	do{\
 	if(!(cond)){\
-	LogMgr::Obj().PrintfCond(LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "condition fail \[" #cond "]. ", ##__VA_ARGS__); \
+	su::LogMgr::Obj().PrintfCond(su::LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "condition fail \[" #cond "]. ", ##__VA_ARGS__); \
 	return ret;\
 	}	\
-	}while(0)
+	}while(0);
 
 #define COND_R(cond, ret)\
 	do{\
 	if(!(cond)){\
 	return ret;\
 	}	\
-	}while(0)
+	}while(0);
 
 #define COND(cond)\
 	do{\
 	if(!(cond))	\
 	return; \
-	}while(0)
+	}while(0);
 
 #define L_ASSERT(cond)\
 	do{\
 	if(!(cond)){\
-	LogMgr::Obj().Printf(LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "assert error"); \
+	su::LogMgr::Obj().Printf(su::LL_ERROR, __FILE__, __LINE__, __FUNCTION__, "assert error"); \
 	}	\
-	}while(0)
+	}while(0);
 
 
 
