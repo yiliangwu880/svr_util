7996358c1e461a9404cd86e8abe3deb4a726d513
yiliangwu <yiliang@qq.com>
2019-09-16 10:22:03 +0800

    add task

--- task.lua
+++ task.lua
@@ -1,4 +1,12 @@
 
+#define DEBUG_ASSERT(pointer, info, ...)	\
+	if(!pointer)	\
+	{	\
+		OptErrorLog(info);	\
+		return __VA_ARGS__;	\
+	}
+参考__VA_ARGS__写法，弄个日志返回宏
+
 SingleProgress 加更容易复用层。
 
 fun()
