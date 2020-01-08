37bc6e4e38556d6efa2327eefb8c69e6220d0931
yiliangwu880 <30390012@qq.com>
2020-01-08 19:20:26 +0800

    add delay opt

---
+++ include/game_util/delay_handler.h
@@ -0,0 +1,180 @@
+/*
+author:yiliangwu880
+you can get more refer from https://github.com/yiliangwu880/CppUtility.git
+brief: 延时处理逻辑， 比如:处理离线玩家事件，缓存操作，等读取数据库成功后执行。
+例子：
+	class MyOptMgr: public DelayOptMgr<MyTarget, uint64>
+	{
+	public:
+		virtual MyTarget *OnFindTarget(uint64 target_id)		//实现查找内存目标	
+		{
+			...
+		}
+
+		virtual void OnMissTarget(uint64 target_id)			//实现，请求读档。  (缓存没有对象时会调用）
+		{
+			...
+		}
+	};
+
+	MyOptMgr mgr;
+
+	mgr.OptTarget(id, target);                                          //3 读档成功 
+	mgr.DelOpt(id);                                                     //4 读档失败调用
+
+	                                                                    //5 加入操作请求
+	auto opt = [&](TargetType &target){};                                          
+	mgr.AddOpt(target_id, opt);
+
+	或者
+
+	auto opt = std::bind(bindFun, _1, ...);
+	mgr.AddOpt(target_id, opt);
+
+	或者 
+
+	auto opt = std::bind(&TargetType::Fun, _1, ...);//指定成员函数
+	mgr.AddOpt(target_id, opt);
+
+*/
+
+#pragma once
+#include "../typedef.h"
+#include <vector>
+#include <map>
+#include <utility>
+#include <functional>
+
+                                                                        //操作目标
+
+//@Obj			 被操作的对象
+//@ObjId		 对象的 id
+//@MAX_OPT_NUM   每个对象最大缓存函数对象数
+template<class Obj, class ObjId=uint64, const uint32 MAX_OPT_NUM = 3>
+class  DelayOptMgr
+{
+	using OptFun = std::function<void(Obj &)>;
+	typedef std::vector<OptFun> VecDelayOpt;
+	typedef std::map<ObjId, VecDelayOpt> Id2Vec;
+
+	bool m_is_opting = nullptr;				                            //true表示进入BaseDelayOptMgr::OptTarget运行中
+	Id2Vec m_id_2_vec;		                                            //id 2 vec, vec ==opt list
+public:
+	                                                                    //加一个操作.		(目标找到马上执行，不在就等调用  HandleTarget 再操作)
+	void AddOpt(ObjId target_id, OptFun opt);
+	                                                                    //对目标操作缓存操作
+	void OptTarget(ObjId target_id, Obj &target);
+
+	void DelOpt(ObjId target_id);								        //调用删除目标缓存操作  (读档失败，目标不存时调用)
+
+	 //for test use
+	uint32 GetMaxNum() const {return MAX_OPT_NUM;}
+	uint32 GetOptNum(ObjId target_id);
+private:
+    virtual Obj *OnFindTarget(ObjId target_id)=0;			//实现查找内存目标
+	virtual void OnMissTarget(ObjId target_id) = 0;				    //实现，请求读档。  (缓存没有对象时会调用）
+
+
+public:
+
+};
+
+template<class Obj, class ObjId, const uint32 MAX_OPT_NUM>
+void DelayOptMgr<Obj, ObjId, MAX_OPT_NUM>::OptTarget(ObjId target_id, Obj &target)
+{
+	if (m_is_opting)
+	{
+		return;//跑这里属于递归调用，退出不用处理。第一次调用进来的函数会处理
+	}
+	int cnt = 0;
+	while (true)
+	{
+		m_is_opting = true;
+		//////////////测试错误代码，后期没错可以删掉//////////
+		cnt++;
+		if (cnt > 100)
+		{
+			printf("error,  endless loop?\n");
+		}
+		////////////////////////////////////////
+		auto it = m_id_2_vec.find(target_id);
+		if (it == m_id_2_vec.end())
+		{
+			m_is_opting = false;
+			return;
+		}
+		//复制一份,避免直接引用 m_id_2_vec 导致下面递归错误
+		VecDelayOpt vec_opt;
+		vec_opt.swap(it->second);
+		m_id_2_vec.erase(it);
+
+		for(OptFun opt: vec_opt)
+		{
+			if (nullptr == opt)
+			{
+				printf("error, why save null point?\n");
+				continue;
+			}
+			opt(target);//里面会可能继续调用AddOpt，给m_id_2_vec[id]加成员
+		}
+	}
+	m_is_opting = false;
+}
+
+template<class Obj, class ObjId, const uint32 MAX_OPT_NUM>
+void DelayOptMgr<Obj, ObjId, MAX_OPT_NUM>::AddOpt(ObjId target_id, OptFun opt)
+{
+	if (nullptr == opt || 0 == target_id)
+	{
+		printf("error para \n");
+		return;
+	}
+
+	VecDelayOpt &vec_opt = m_id_2_vec[target_id];
+	if (vec_opt.size() >= MAX_OPT_NUM)
+	{
+		//缓存操作太多了		
+		printf("error, req is too more, id=%lld\n", target_id);
+		return;
+	}
+
+	vec_opt.push_back(opt);
+
+	if (Obj *pTarget = OnFindTarget(target_id))
+	{
+		OptTarget(target_id, *pTarget);
+	}
+	else
+	{
+		//req get target from db
+		if (vec_opt.size() == 1) //第一个操作才请求读库
+		{
+			OnMissTarget(target_id);
+		}
+	}
+}
+
+
+template<class Obj, class ObjId, const uint32 MAX_OPT_NUM>
+void DelayOptMgr<Obj, ObjId, MAX_OPT_NUM>::DelOpt(ObjId target_id)
+{
+	typename Id2Vec::iterator it = m_id_2_vec.find(target_id);
+	if (it == m_id_2_vec.end())
+	{
+		return;
+	}
+
+	m_id_2_vec.erase(it);
+}
+
+template<class Obj, class ObjId, const uint32 MAX_OPT_NUM>
+uint32 DelayOptMgr<Obj, ObjId, MAX_OPT_NUM>::GetOptNum(ObjId target_id)
+{
+	typename Id2Vec::iterator it = m_id_2_vec.find(target_id);
+	if (it == m_id_2_vec.end())
+	{
+		return 0;
+	}
+	VecDelayOpt &vec_opt = it->second;
+	return vec_opt.size();
+}
---
+++ test/test_delay_handler.cpp
@@ -0,0 +1,192 @@
+
+#include "game_util/delay_handler.h"
+#include "cnt_typedef.h"
+#include "unit_test.h"
+
+using namespace std;
+using namespace su;
+using namespace placeholders;
+
+#define WHILE_NUM(num)\
+    for(unsigned int i=0; i<num; ++i)
+//简化stl迭代写法
+#define FOR_IT(type, ctn)\
+    for(type::iterator it=(ctn).begin(); it!=(ctn).end(); ++it)
+//C++11才支持
+#define FOR_IT_(ctn)\
+    for(auto it=(ctn).begin(); it!=(ctn).end(); ++it)
+
+#define CONST_FOR_IT(type, ctn)\
+    for(type::const_iterator it=(ctn).begin(); it!=(ctn).end(); ++it)
+namespace
+{
+	class MyTarget
+	{
+	public:
+		MyTarget()
+		{
+			m_opt1_cnt = m_opt2_cnt = 0;
+		}
+		void Fun()
+		{
+			m_opt3_cnt++;
+		}
+		uint32 m_opt1_cnt = 0;	//操作 map 次数 
+		uint32 m_opt2_cnt = 0;	//操作 map 次数 
+		uint32 m_opt3_cnt = 0;
+	};
+
+	typedef std::map<uint64, MyTarget> Id2Target;
+	Id2Target g_id_2_target;	//内存目标
+	Id2Target g_db_id_2_target; //db目标
+	VecUint64 g_req_db_id_ls;     //模拟请求DB的id
+
+	class MyOptMgr: public DelayOptMgr<MyTarget, uint64>
+	{
+	public:
+		virtual MyTarget *OnFindTarget(uint64 target_id)			//实现查找内存目标
+		{
+			Id2Target::iterator it = g_id_2_target.find(target_id);
+			if (it == g_id_2_target.end())
+			{
+				return NULL;
+			}
+			return &(it->second);
+		}
+
+		virtual void OnMissTarget(uint64 target_id)			//实现，请求读档。  (缓存没有对象时会调用）
+		{
+			g_req_db_id_ls.push_back(target_id);
+		}
+	};
+
+	MyOptMgr g_opt_mgr;
+
+	void bindFun(MyTarget &my_target, uint32 add)
+	{
+		my_target.m_opt1_cnt += add;
+	}
+	void test1()
+	{
+		UNIT_INFO("-----------print error is ok---------------");
+		auto  opt1 = [](MyTarget &obj){obj.m_opt1_cnt++;};	
+		auto  opt2 = [](MyTarget &obj){obj.m_opt2_cnt++;};
+		g_opt_mgr.AddOpt(1, opt1);
+		g_opt_mgr.AddOpt(1, opt1);// opt1非法参数报错
+		g_opt_mgr.AddOpt(1, NULL);// NULL非法参数报错
+		g_opt_mgr.AddOpt(0, opt2);// opt2非法参数报错
+
+		UNIT_ASSERT(2 == g_opt_mgr.GetOptNum(1));
+
+		UNIT_INFO("-----------print error is ok---------------");
+		WHILE_NUM(g_opt_mgr.GetMaxNum() + 1)
+		{
+			g_opt_mgr.AddOpt(2, opt1);//缓存溢出报错
+		}
+
+
+		FOR_IT(VecUint64, g_req_db_id_ls)
+		{
+			uint64 id = *it;
+			g_id_2_target[id] = MyTarget();
+			MyTarget &target = g_id_2_target[id];
+			g_opt_mgr.OptTarget(id, target);
+		}
+		g_req_db_id_ls.clear();
+
+		UNIT_INFO("----------------end---------------");
+
+		//递归调用
+		g_id_2_target[1] = MyTarget();
+
+		auto opt = [](MyTarget &my_target)
+		{
+			my_target.m_opt2_cnt++;
+			auto  opt1 = [](MyTarget &obj) {obj.m_opt1_cnt++; };
+			g_opt_mgr.AddOpt(1, opt1);
+		};
+		uint32 old_cnt = g_id_2_target[1].m_opt1_cnt;
+		g_opt_mgr.AddOpt(1, opt);
+		UNIT_ASSERT(old_cnt + 1 == g_id_2_target[1].m_opt1_cnt);
+
+		//std::bind
+		{
+			uint32 old_cnt = g_id_2_target[1].m_opt1_cnt;
+			auto f = std::bind(bindFun, _1, 3);
+			g_opt_mgr.AddOpt(1, f);
+			UNIT_ASSERT(old_cnt + 3 == g_id_2_target[1].m_opt1_cnt);
+		}
+		{
+			auto f = std::bind(&MyTarget::Fun, _1);
+			//g_opt_mgr.AddOpt(1, f);
+			f(g_id_2_target[1]);
+			UNIT_ASSERT(1 == g_id_2_target[1].m_opt3_cnt);
+		}
+	}
+
+	void test2()
+	{
+		//延时请求
+		g_id_2_target.clear();
+		g_req_db_id_ls.clear();
+		auto  opt1 = [](MyTarget &obj) {obj.m_opt1_cnt++; };
+		auto  opt2 = [](MyTarget &obj) {obj.m_opt2_cnt++; };
+
+		g_opt_mgr.AddOpt(1, opt1);
+		g_opt_mgr.AddOpt(1, opt2);
+		UNIT_ASSERT(2== g_opt_mgr.GetOptNum(1));
+		g_opt_mgr.DelOpt(1);
+		UNIT_ASSERT(0 == g_opt_mgr.GetOptNum(1));
+
+		g_opt_mgr.AddOpt(1, opt1);
+		g_opt_mgr.AddOpt(1, opt2);
+		UNIT_ASSERT(2== g_opt_mgr.GetOptNum(1));
+
+		//读库模拟
+		FOR_IT(VecUint64, g_req_db_id_ls)
+		{
+			uint64 id = *it;
+			if (g_id_2_target.find(id) == g_id_2_target.end())
+			{
+				g_id_2_target[id]= MyTarget();
+			}
+			MyTarget &target = g_id_2_target[id];
+			g_opt_mgr.OptTarget(id, target);
+		}
+		UNIT_ASSERT(0 == g_opt_mgr.GetOptNum(1));
+		UNIT_ASSERT(1 == g_id_2_target[1].m_opt1_cnt);
+		UNIT_ASSERT(1 == g_id_2_target[1].m_opt2_cnt);
+
+		//操作在内存对象
+		g_opt_mgr.AddOpt(1, opt1);
+		g_opt_mgr.AddOpt(1, opt2);
+		UNIT_ASSERT(0== g_opt_mgr.GetOptNum(1));
+		UNIT_ASSERT(2 == g_id_2_target[1].m_opt1_cnt);
+		UNIT_ASSERT(2 == g_id_2_target[1].m_opt2_cnt);
+
+		{
+			g_id_2_target[2] = MyTarget();
+			UNIT_ASSERT(0 == g_id_2_target[2].m_opt1_cnt);
+			g_opt_mgr.AddOpt(2, opt1);
+			UNIT_ASSERT(1 == g_id_2_target[2].m_opt1_cnt);
+		}
+	}
+	
+	void test3()
+	{
+		WHILE_NUM(1000)
+		{
+			test2();
+		}
+	}
+}
+
+
+UNITTEST(test_delay_handler)
+{
+	test1();
+	test2();
+	test3();
+	UNIT_INFO("---test_delay_handler end---");
+
+}
