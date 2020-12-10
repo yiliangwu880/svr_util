298f605526404a2291cd9ae7e971dfbccd66a3d9
wuyiliang <30390012@qq.com>
2020-12-10 10:32:11 +0800

    升级逻辑代码优化

--- src/game_util/exp_lvup.cpp
+++ src/game_util/exp_lvup.cpp
@@ -23,7 +23,33 @@ void LvupBase::AddExp( uint64 addNum )
 		needExp = GetNextLvNeedExp(m_lv+1);
 	};
 }
+//未测试，该这样代码容易读点
+void LvupBase::AddExpNew( uint64 addNum )
+{
+	uint32 max_lv = GetMaxLv();
+	if (m_lv >= max_lv)
+	{
+		return;
+	}
+	m_exp+=addNum;
 
+	while (true)
+	{
+		if (m_lv >= max_lv)
+		{
+			break;
+		}
+		uint64 needExp = GetNextLvNeedExp(m_lv+1);
+		if(m_exp < needExp)
+		{
+			break
+		}
+
+		m_exp -= needExp;
+		m_lv++;
+		OnLvup();
+	};
+}
 void LvupBase::SetLv(uint32 lv)
 {
 	uint32 max_lv = GetMaxLv();
