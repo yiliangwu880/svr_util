8e8240be5b0bb6e8174d8ca67069e5da58c2ef4a
yiliangwu880 <30390012@qq.com>
2021-05-19 17:53:56 +0800

    typedef

--- include/typedef.h
+++ include/typedef.h
@@ -2,11 +2,10 @@
 brief:常用基本类型定义
 */
 #pragma once
-
-using uint16 = unsigned short;
-using uint32 = unsigned int;
-using uint64 = unsigned long long;
-using int64 = long long;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+using int64 = int64_t;
 using uint8 = unsigned char;
 using int32 = int;
 
